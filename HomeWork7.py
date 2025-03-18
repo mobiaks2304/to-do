76c7d61680f5bf9984d8497c695fc63e4e85f71c
mobiaks2304 <lotockiy.matviy@gmail.com>
2025-03-18 17:48:28 +0200

    update і csv

--- HomeWork7.py
+++ HomeWork7.py
@@ -1,3 +1,5 @@
+import csv
+
 to_do = []
 
 while True:
@@ -5,9 +7,10 @@ while True:
     player_input = input(':')
 
     if player_input == '1':
-        add = input('введіть завдання:')
-        to_do.append(add)
-    
+        add = (input('введіть завдання:'))
+        new_task = [add, 'невиконано']
+        to_do.append(new_task)
+        print(to_do)
     if player_input == '2':
         try:    
             remove = int(input('введіть номер завдання яке хочете видалити:'))
@@ -17,5 +20,7 @@ while True:
     
     if player_input == '3':
         print(to_do)
-        
 
+    with open('todo.csv', mode = 'a',encoding='UTF-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(to_do)
