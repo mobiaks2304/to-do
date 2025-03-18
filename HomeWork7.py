34d5dfe439ebf4fcff5773eb588eab7180063204
mobiaks2304 <lotockiy.matviy@gmail.com>
2025-03-18 17:21:31 +0200

    видалення завдання

--- HomeWork7.py
+++ HomeWork7.py
@@ -1,10 +1,18 @@
-to_do = {}
+to_do = []
 
-# while True:
-#     print("1.Додати завдання 2.Видалити завдання \n 3.Позначення завдання як виконане 4.видалити  завдання 5.нажміть 'q' щоб вийти")
-#     player_input = input(':')
+while True:
+    print("1.Додати завдання 2.Видалити завдання \n 3.Позначення завдання як виконане 4.видалити  завдання 5.нажміть 'q' щоб вийти")
+    player_input = input(':')
 
-#     if player_input == '1':
-#         add = input('введіть завдання:')
-#         to_do[add] = 'невиконане'
-print(to_do)
+    if player_input == '1':
+        add = input('введіть завдання:')
+        to_do.append(add)
+    
+    if player_input == '2':
+        remove = int(input('введіть номер завдання яке хочете видалити:'))
+        to_do.remove(remove - 1)
+        
+    
+    
+
+    
