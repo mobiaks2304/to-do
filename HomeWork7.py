508e27a5bdc5e1677da85a2677c56216b455d855
mobiaks2304 <lotockiy.matviy@gmail.com>
2025-03-18 18:32:19 +0200

    перегляд завдань

--- HomeWork7.py
+++ HomeWork7.py
@@ -2,11 +2,18 @@
 to_do = []
  
 while True:
-    print("1.Додати завдання 2.Видалити завдання \n 3.Позначення завдання як виконане 4.видаляння завдання 5.нажміть 'q' щоб вийти")
+    print("1.Додати завдання 2.Видалити завдання 3.перегляд завдань \n 4.Позначення завдання як виконане 5.нажміть 'q' щоб вийти")
     player_input = input(':')
 
     if player_input == '1':
         new_task = input('введіть нове завдання:')
-        add = [new_task, 'невиконане']
-        to_do.append(add)
+        
+        to_do.append(new_task)
+        
+    if player_input == '2':
+        delete = input('введіть номер завдання яке хочете видалити:')
+        to_do.remove(delete - 1)
+    
+    if player_input == '3':
         print(to_do)
+
