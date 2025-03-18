bfa578ddfd2e376ae6f5cc5c704f176a307f2d0e
mobiaks2304 <lotockiy.matviy@gmail.com>
2025-03-18 17:26:34 +0200

    перегляд завдань

--- HomeWork7.py
+++ HomeWork7.py
@@ -1,7 +1,7 @@
 to_do = []
 
 while True:
-    print("1.Додати завдання 2.Видалити завдання \n 3.Позначення завдання як виконане 4.видалити  завдання 5.нажміть 'q' щоб вийти")
+    print("1.Додати завдання 2.Видалити завдання 3.перегляд завдань \n 4.Позначення завдання як виконане  5.нажміть 'q' щоб вийти")
     player_input = input(':')
 
     if player_input == '1':
@@ -9,10 +9,13 @@ while True:
         to_do.append(add)
     
     if player_input == '2':
-        remove = int(input('введіть номер завдання яке хочете видалити:'))
-        to_do.remove(remove - 1)
+        try:    
+            remove = int(input('введіть номер завдання яке хочете видалити:'))
+            to_do.remove(remove - 1)
+        except ValueError:
+            print('введи правильне значення')
+    
+    if player_input == '3':
+        print(to_do)
         
-    
-    
 
-    
